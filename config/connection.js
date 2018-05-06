9c017caf90e986e06ef52df742daf0470568b7bc
Calle Lacouture <celacouture@gmail.com>
2018-05-05 22:04:40 -0500

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,31 @@
 var mysql=require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL){
+  connection=mysql.createConnection(process.env.JAWS_URL);
+}else{
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
 
-connection.connect(function(err){
-  if(err){
-    console.error("error connecting " + err.stack);
-    return;
-  }
-  console.log("connected as id " +connection.threadId );
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
+//
+// connection.connect(function(err){
+//   if(err){
+//     console.error("error connecting " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " +connection.threadId );
+// });
+
+connection.connect();
 module.exports=connection;
